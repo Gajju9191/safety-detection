24ec385b10e157118e5c535d9175b9de75442a32
Gajju9191 <gajananw9191@gmail.com>
2026-02-01 00:06:25 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Gajju',
+    author_email= 'gajananw9191@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
